6dbf50150706ec7928bfd20dca743aa707932167
LeFayMasa <r01.gm.2190192@da.iwasaki.ac.jp>
2021-03-13 07:53:17 +0900

    {Stract.h}を更新しました

--- Stract.h
+++ Stract.h
@@ -1,14 +1,27 @@
 #pragma once
 //Ballに関する構造体
 typedef struct {
-	int cnt, knd;//カウンタ、種類
-	bool flag;//フラグ
-	int x, y, r, angle, spd_x, spd_y;//座標、半径、角度、スピードx&y
-}ball_t;
+	int   knd;		//種類
+	bool  flag;		//フラグ
+	float x,		//Ball_X_座標
+		  y,		//Ball_Y_座標
+		  r,		//Ball_半径
+		  angle,	//Ball_角度
+		  spd_x,	//スピードx
+		  spd_y;	//スピードy
+}Ball_t;
 
 //Blockに関する構造体
 typedef struct {
-	int cnt, knd;//カウンタ、種類
-	int x, y;//座標
-}block_t;
+	int   knd;		//種類
+	bool  flg;		//フラグ	
+	float x,		//Block_X座標
+		  y;		//Block_Y座標
+}Block_t;
 
+//BlackHoleに関する構造体
+typedef struct {
+	float x,        //BH_X_中心座標
+		  y,        //BH_Y_中心座標
+		  r;        //BH_半径
+}BH_t;
