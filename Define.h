2ee3427484b9d710d64af8b8c95fdc944891516a
2A_24_佐藤翼 <r01.gm.2190280@da.iwasaki.ac.jp>
2021-03-18 14:03:00 +0900

    //GV.hの内容とDefine.hの内容を入れ替え

--- Define.h
+++ Define.h
@@ -1,13 +1,1 @@
 #pragma once
-
-/*‚±‚±‚Í•K—p*/
-#include "DxLib.h"
-#include "math.h"
-#include "define.h"
-
-
-#ifdef GLOBAL_INSTANCE
-#define GLOBAL
-#else
-#define GLOBAL extern 
-#endif
--- GV.h
+++ GV.h
@@ -1,1 +1,12 @@
 #pragma once
+/*‚±‚±‚Í•K—p*/
+#include "DxLib.h"
+#include "math.h"
+#include "define.h"
+
+
+#ifdef GLOBAL_INSTANCE
+#define GLOBAL
+#else
+#define GLOBAL extern 
+#endif
